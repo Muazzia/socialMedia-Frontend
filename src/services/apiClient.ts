02d49611026f85832317081707f7d360d13e5bd4
Muaz <you@example.com>
2023-10-29 18:32:12 +0500

    last

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+const staticUrlPath = "https://social-backend.adaptable.app";
 const api = axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://social-backend.adaptable.app",
+  baseURL: staticUrlPath,
 });
 
-const staticUrlPath = "https://social-backend.adaptable.app";
 export { staticUrlPath };
 
 export default api;
